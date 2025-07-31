a0a29f7f1356ef970a439d2f5718e5bff6ec795a
Witold <witold696@gmail.com>
2025-07-31 15:02:40 +0200

    Initial commit

---
+++ src/main/java/org/example/evestore/EveStoreApplication.java
@@ -0,0 +1,13 @@
+package org.example.evestore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EveStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EveStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/evestore/EveStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.evestore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EveStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
